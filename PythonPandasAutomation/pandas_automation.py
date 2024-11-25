9b58cc6b495e10ddba7ad0bb1a8d6dd6e7a9a69c
rooftop-kev <100609668+rooftop-kev@users.noreply.github.com>
2024-11-25 00:14:03 -0800

    Updating file direction: Analyze used car list

--- PythonPandasAutomation/pandas_automation.py
+++ PythonPandasAutomation/pandas_automation.py
@@ -1,2 +1,10 @@
 #practice course material here
-commands = ['import pandas as pd', 'pd.read_csv()']
+import pandas as pd
+
+#this is the entire list of cars
+used_cars_list = pd.read_csv('used_car_dataset.csv')
+
+#cars from 2023
+used_cars_from_2023 = used_cars_list[used_cars_list['Year']>2023].head(5)
+
+print(used_cars_from_2023)
